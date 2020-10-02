d3fb8ff8c626206e3c89ce544144bc46e22f637d
18733 <jiatonghan@cmail.carleton.ca>
2020-10-02 00:01:43 -0400

    Third

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,17 +1,21 @@
 import java.util.HashMap;
-import java.util.TooManyListenersException;
+
 
 public class AddressBook {
+
     private HashMap<String, BuddyInfo> find;
     public AddressBook() {
         find = new HashMap<String, BuddyInfo>();
     }
+
     public void addBuddy(BuddyInfo a) {
         find.put(a.getName(),a);
     }
+
     public void removeBuddy(BuddyInfo a){
         find.remove(a.getName());
     }
+
     public static void main (String[] args){
         BuddyInfo buddy=new BuddyInfo("Tom","Carleton","613");
         AddressBook addressBook=new AddressBook();
