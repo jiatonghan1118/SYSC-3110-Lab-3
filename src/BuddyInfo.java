366757011916610dd8a3aaf9182aa6b57dbc08a4
18733 <jiatonghan@cmail.carleton.ca>
2020-10-02 00:14:24 -0400

    2020.10.01 second

---
+++ src/BuddyInfo.java
@@ -0,0 +1,22 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private String phoneNum;
+
+    public BuddyInfo(String name, String address, String phoneNum){
+        this.name=name;
+        this.address=address;
+        this.phoneNum=phoneNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+
